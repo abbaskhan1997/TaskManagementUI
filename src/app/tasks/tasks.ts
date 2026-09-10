e4b8e60376ed278b316d21f81a221fb22848e110
abbaskhan1997 <abbaskhancs88@gmail.com>
2026-09-10 19:15:23 +0500

    Add task search UI

--- src/app/tasks/tasks.ts
+++ src/app/tasks/tasks.ts
@@ -3,9 +3,10 @@ import { TaskService } from '../services/task-service';
 import { OnInit } from '@angular/core';
 import { ChangeDetectorRef } from '@angular/core';
 import { Router } from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 @Component({
-  imports: [],
+  imports: [FormsModule],
   selector: 'app-tasks',
   styleUrl: './tasks.css',
   templateUrl: './tasks.html',
@@ -16,11 +17,21 @@ export class Tasks implements OnInit {
     private cdr: ChangeDetectorRef,
     private router: Router,
   ) {}
-
+  searchText: string = '';
   tasks: any[] = [];
+
   ngOnInit() {
     this.loadTasks();
   }
+
+  get filteredTasks() {
+  return this.tasks.filter(task =>
+    task.title.toLowerCase().includes(this.searchText.toLowerCase()) ||
+    task.description.toLowerCase().includes(this.searchText.toLowerCase()) ||
+    task.status.toLowerCase().includes(this.searchText.toLowerCase()) ||
+    task.priority.toLowerCase().includes(this.searchText.toLowerCase())
+  );
+}
 
   loadTasks() {
     this.taskService.getTasks().subscribe((tasks: any) => {
